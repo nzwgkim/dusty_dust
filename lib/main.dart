d29b5b01bc3b7fbec53d357734b14686425a3cbd
Woody <nz.wgkim@gmail.com>
2023-02-24 02:44:03 +1300

    프로젝트에 Hive 적용하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+late Box box;
+void main() async {
+  await Hive.initFlutter();
+
+  box = await Hive.openBox('testBox');
+
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
+  for (ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox<StatModel>(itemCode.name);
+  }
+
   runApp(const MyApp());
 }
 
