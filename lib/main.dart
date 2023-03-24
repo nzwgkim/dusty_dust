c22c54416a7d23bec94f12bdb13a844a8471641c
Woody <nz.wgkim@gmail.com>
2023-03-24 17:04:16 +1300

    Revert "프로젝트에 Hive 적용하기"
    This reverts commit d29b5b01bc3b7fbec53d357734b14686425a3cbd.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-late Box box;
-void main() async {
-  await Hive.initFlutter();
-
-  box = await Hive.openBox('testBox');
-
-  Hive.registerAdapter<StatModel>(StatModelAdapter());
-  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
-
-  for (ItemCode itemCode in ItemCode.values) {
-    await Hive.openBox<StatModel>(itemCode.name);
-  }
-
+void main() {
   runApp(const MyApp());
 }
 
