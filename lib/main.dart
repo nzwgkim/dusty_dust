3ae49ab8fc8f4e97869880f89fb5ba7cfbef141d
Woody <nz.wgkim@gmail.com>
2023-03-24 16:00:44 +1300

    아답터 생성 및 적용, adaptor 등록 및 itemCode 각각에 대해 openBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -8,6 +9,13 @@ const testBox = 'test';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(testBox);
+
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
+  for (ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
 
   runApp(MaterialApp(
     title: 'Flutter Demo',
